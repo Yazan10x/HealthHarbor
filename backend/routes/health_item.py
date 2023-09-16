0d685a3f72793dc1ae5ebb1adef942a75849d859
vincent-t-wan <wanv2002@gmail.com>
2023-09-16 15:09:39 -0400

    fixed endpoint json formats

--- backend/routes/health_item.py
+++ backend/routes/health_item.py
@@ -29,26 +29,19 @@ def get_items() -> Response:
 
 @health_item.route("/create_item", methods=['POST'])
 def create_item() -> Response:
-
-    item =  '{ "id": "35998002-7e37-443f-ab43-e4ed7517dce9", "label":"hi", "quantity":1000, "description": "htn2023"}'
-    item_dict = json.loads(item)
-    create(item_dict)
-    return item, status.HTTP_200_OK
+    item_json = request.get_json()
+    create(item_json)
+    return item_json, status.HTTP_200_OK
 
 
 @health_item.route("/update_item", methods=['POST'])
 def update_item() -> Response:
-
-    item =  '{ "id": "35998002-7e37-443f-ab43-e4ed7517dce9", "label":"hello", "quantity":200, "description": "11111111"}'
-    item_dict = json.loads(item)
-    update(item_dict)
-
-    # update(request.get_json())
-    return item, status.HTTP_200_OK
+    item_json = request.get_json()
+    update(item_json)
+    return item_json, status.HTTP_200_OK
 
 
 @health_item.route("/delete_item/<item_id>", methods=['POST'])
 def delete_item(item_id: str) -> Response:
     delete(item_id)
     return item_id, status.HTTP_200_OK
-
