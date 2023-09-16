47d1b6f527bff5b6a322446802a4a5f89a62d0fa
Humza Iqbal <humza.iqbal@mail.utoronto.ca>
2023-09-16 14:47:38 -0400

    Cohere Backend

---
+++ backend/cohere_api/cohere_api.py
@@ -0,0 +1,27 @@
+
+return_sample = [
+    {
+        'rank': 1,
+        'medicine': 'tynanol',
+        'disease': 'headache',
+        'confindence': 0.33
+    },
+    {
+        'rank': 2,
+        'medicine': 'tynanol',
+        'disease': 'headache',
+        'confindence': 0.24
+
+    },
+    {
+        'rank': 3,
+        'medicine': 'tynanol',
+        'disease': 'headache',
+        'confindence': 0.12
+
+    },
+]
+    
+
+def get_disease_with_medicine(dscr: str) -> list[dict[str, str]]:
+    pass
