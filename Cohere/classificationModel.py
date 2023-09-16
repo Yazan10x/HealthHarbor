af655a364b641e1cbd2cf11deb9f75f8b1ca4e91
Humza Iqbal <humza.iqbal@mail.utoronto.ca>
2023-09-16 11:49:57 -0400

    sorted Labels

--- Cohere/classificationModel.py
+++ Cohere/classificationModel.py
@@ -1,14 +1,20 @@
 import cohere
 from cohere.responses.classify import Example
+from cohere.responses.classify import LabelPrediction
+# from cohere.responses.classify import Classification
+
+
 
 import pandas as pd
 import numpy as np
 from imblearn.over_sampling import RandomOverSampler
+from imblearn.under_sampling import RandomUnderSampler
+import heapq
 co = cohere.Client('dW7n7345zsD7VRvluLFqZbRZLtMZfdm3Zf7CS0DA')
 
 # we want to first oversample our dataframe model, then randomly sample 2500 rows to obtain our feature data.
 
-df = pd.read_csv("Cohere\datasets\drugs_side_effects_drugs_com.csv", usecols=['medical_condition', 'medical_condition_description'], nrows=500)
+df = pd.read_csv("Cohere\datasets\drugs_side_effects_drugs_com.csv", usecols=['medical_condition', 'medical_condition_description'])
 
 # df.drop(df[df["drug_name"]=="Accutane"].index)
 
@@ -18,7 +24,7 @@ examples =[]
 X = df[df.columns[:-1]].values
 y = df[df.columns[-1]].values
 
-over = RandomOverSampler()
+over = RandomUnderSampler()
 X, y = over.fit_resample(X, y)
 data = np.hstack((X, np.reshape(y, (-1, 1))))
 transformed_df = pd.DataFrame(data, columns=df.columns)
@@ -29,7 +35,7 @@ for index, row in transformed_df.iterrows():
     examples.append(Example(row["medical_condition_description"], row["medical_condition"]))
 
 inputs=[
-  "Big red spots on body", "Really stressed about exams. Want to Kill myself."#, "Hand is spazzing out"
+  "Big red spots on body"#, "Really stressed about exams. Want to Kill myself.", "pain in left arm"
 ]
 
 # print(examples)
@@ -38,4 +44,30 @@ response = co.classify(
   inputs=inputs,
   examples=examples,
 )
-print(response)
+print(response, "\n\n")
+
+lol = response[0].labels.items
+
+for i in response:
+    print(1)
+
+LabelPrediction
+
+LabelsDict = {}
+
+for i in range(len(response)):
+    for j in response[i].labels:
+      x = response[i].labels[j]
+      LabelsDict[j] = x.confidence
+
+top = heapq.nlargest(3, LabelsDict.values())
+topnames = []
+for i in LabelsDict.keys():
+    if LabelsDict[i] in top:
+        topnames.append((i, LabelsDict[i]))
+
+print(topnames)
+print(top)
+
+#print(responseww)
+#print(response)
