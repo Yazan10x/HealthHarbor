2cc41e03eaf6abc0bc3b45a355f5ff97ad837de9
Yazan Armoush <yazan.armoush1@gmail.com>
2023-09-29 23:51:20 -0400

    Removed Old DB

--- backend/routes/cockroach/utilities.py
+++
@@ -1,5 +0,0 @@
-# adds a string str_add behind every character in str that is in chars
-def add_string(str: str, chars: str, str_add: str): 
-    for c in chars:
-        s = str.replace(c, str_add + c)
-    return s
